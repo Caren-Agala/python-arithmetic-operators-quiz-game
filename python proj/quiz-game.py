b2a8b29156ef9ecc3f3439bf3c7962c248a58085
Caren-Agala <caren.khavukui@students.jkuat.ac.ke>
2023-02-20 15:35:09 +0300

    python arithmetic operators quiz game

---
+++ python proj/quiz-game.py
@@ -0,0 +1,59 @@
+import pdb
+import time
+print("Welcome to my math quiz!")
+
+playing = input("Do you want to play the game? (y/n): ")
+score = 0
+
+if playing != "y":
+    quit()
+print("Sure! Let\'s play : )") 
+
+time.sleep(1)
+
+answer = input("What is 4**2? ")
+if answer == "16":
+    print("Correct!")
+    score += 1
+    print("Score: " + str(score))
+else:
+    print("Incorrect!")
+    score = score
+    print("Score: " + str(score))
+time.sleep(1)    
+
+answer = input("What is 4*2? ")
+if answer == "8":
+    print("Correct!")
+    score += 1
+    print("Score: " + str(score))
+else:
+    print("Incorrect!")
+    score = score
+    print("Score: " + str(score))
+time.sleep(1)   
+    
+answer = input("What is 4/2? ")
+if answer == "2":
+    print("Correct!")
+    score += 1
+    print("Score: " + str(score))
+else:
+    print("Incorrect!")
+    score = score
+    print("Score: " + str(score))
+time.sleep(1)   
+    
+answer = input("What is 4%2? ")
+if answer == "0":
+    print("Correct!")
+    score += 1
+    print("Score: " + str(score))
+else:
+    print("Incorrect!")
+    score = score
+    print("Score: " + str(score))
+time.sleep(1)
+
+print("Your final score is: " + str(score))
+print("Congratulations!")
